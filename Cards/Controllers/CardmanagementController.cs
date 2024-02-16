0fcc34af4080c0d71485f5d918a7467b5f2d6b9f
Francis Kingori <franciskingori448@gmail.com>
2024-02-16 19:08:30 +0300

    update

--- Cards/Controllers/CardmanagementController.cs
+++ Cards/Controllers/CardmanagementController.cs
@@ -2,10 +2,8 @@
 using DBL.Entities;
 using DBL.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace Cards.Controllers
 {
@@ -24,14 +22,14 @@ namespace Cards.Controllers
         #region Card Management
         [HttpGet("Getallsystemcard")]
         [Authorize(Roles = "Admin")]
-        public async Task<ActionResult<IEnumerable<Systemcarddatamodel>>> Getallsystemcard(int Offset,int Count, string? Search)
+        public async Task<ActionResult<IEnumerable<Systemcarddatamodel>>> Getallsystemcard(int Offset = 0, int Count = 10, string? Search = "")
         {
-            var data = await bl.Getallsystemcard(Offset, Count,Search);
+            var data = await bl.Getallsystemcard(Offset, Count, Search);
             return Ok(data);
         }
         [HttpGet("Getusersystemcard/{Userid}")]
         [Authorize(Roles = "Member")]
-        public async Task<ActionResult<IEnumerable<Systemcarddatamodel>>> Getusersystemcard(long Userid,int Offset, int Count, string? Search)
+        public async Task<ActionResult<IEnumerable<Systemcarddatamodel>>> Getusersystemcard(long Userid, int Offset = 0, int Count = 10, string? Search = "")
         {
             var data = await bl.Getusersystemcard(Userid, Offset, Count, Search);
             return Ok(data);
@@ -45,8 +43,8 @@ namespace Cards.Controllers
         [HttpGet("Getsystemcardbycardid/{Userid}/{Cardid}")]
         public async Task<ActionResult<Systemcards>> Getsystemcardbycardid(long Userid, long Cardid)
         {
-            var data = await bl.Getsystemcardbycardid(Userid,Cardid);
-            return Ok(data); 
+            var data = await bl.Getsystemcardbycardid(Userid, Cardid);
+            return Ok(data);
         }
         [HttpPut("Updatesystemcard")]
         public async Task<ActionResult<Genericmodel>> Updatesystemcard([FromBody] Systemcards Systemcard)
@@ -55,10 +53,10 @@ namespace Cards.Controllers
             return Ok(data);
         }
         [HttpDelete("Deletesystemcardbycardid/{Userid}/{Cardid}")]
-        public async Task<ActionResult<Genericmodel>> Deletesystemcardbycardid(long Userid,long Cardid)
+        public async Task<ActionResult<Genericmodel>> Deletesystemcardbycardid(long Userid, long Cardid)
         {
-            var data = await bl.Deletesystemcardbycardid(Userid,Cardid);
-            return Ok(data); 
+            var data = await bl.Deletesystemcardbycardid(Userid, Cardid);
+            return Ok(data);
         }
         #endregion
     }
